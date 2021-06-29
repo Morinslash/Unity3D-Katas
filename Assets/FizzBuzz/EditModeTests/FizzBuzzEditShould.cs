5d1ca9005e51f469ed559585d93cfcfbd8ff5318
Sebastian Kulinski <sebastian.elf.kulinski@gmail.com>
2021-06-29 23:04:42 +0200

    [FizzBuzz] refactor production code to remove naive implementation and replace it with modulus of 3 check

--- Assets/FizzBuzz/EditModeTests/FizzBuzzEditShould.cs
+++ Assets/FizzBuzz/EditModeTests/FizzBuzzEditShould.cs
@@ -1,15 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 public class FizzBuzzEditShould
 {
     [Test]
     public void ProcessReturnNumber1AsString()
     {
-        var systemUnderTest = new GameObject().AddComponent<FizzBuzz>();
+        var systemUnderTest = new GameObject().AddComponent<FizzBuzz.FizzBuzzScripts.FizzBuzz>();
         string expectedResult = "1";
 
         var result = systemUnderTest.Process(1);
@@ -19,7 +16,7 @@ public class FizzBuzzEditShould
     [Test]
     public void ProcessReturnsFizzIf3Provided()
     {
-        var systemUnderTest = new GameObject().AddComponent<FizzBuzz>();
+        var systemUnderTest = new GameObject().AddComponent<FizzBuzz.FizzBuzzScripts.FizzBuzz>();
         string expectedResult = "Fizz";
 
         var result = systemUnderTest.Process(3);
@@ -29,7 +26,7 @@ public class FizzBuzzEditShould
     [Test]
     public void ProcessReturnsFizzIf6Provided()
     {
-        var systemUnderTest = new GameObject().AddComponent<FizzBuzz>();
+        var systemUnderTest = new GameObject().AddComponent<FizzBuzz.FizzBuzzScripts.FizzBuzz>();
         string expectedResult = "Fizz";
 
         var result = systemUnderTest.Process(6);
@@ -39,7 +36,7 @@ public class FizzBuzzEditShould
     [Test]
     public void ProcessReturnsFizzIf9Provided()
     {
-        var systemUnderTest = new GameObject().AddComponent<FizzBuzz>();
+        var systemUnderTest = new GameObject().AddComponent<FizzBuzz.FizzBuzzScripts.FizzBuzz>();
         string expectedResult = "Fizz";
 
         var result = systemUnderTest.Process(9);
--- Assets/FizzBuzz/FizzBuzzScripts/FizzBuzz.cs
+++ Assets/FizzBuzz/FizzBuzzScripts/FizzBuzz.cs
@@ -1,27 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class FizzBuzz : MonoBehaviour
+namespace FizzBuzz.FizzBuzzScripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class FizzBuzz : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public string Process(int number)
-    {
-        if (number == 3 || number == 6 || number == 9)
+        public string Process(int number)
         {
-            return "Fizz";
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            return number.ToString();
         }
-        return number.ToString();
     }
 }
